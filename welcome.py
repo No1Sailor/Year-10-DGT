de4162dab0dbef7edd95e620d1090f6dcaf1c714
No1Sailor <168596679+No1Sailor@users.noreply.github.com>
2024-05-08 10:07:53 +1200

    Add files via upload
    This is a new version of the original file that has more codes on it.

--- welcome.py
+++ welcome.py
@@ -1,3 +1,29 @@
 #My first Python program
 
 print("Hello World!")
+
+#KINDS OF VARIABLES
+string_eg1 ="This is a string"
+interger_eg="7"
+float_eg=7.25
+bool_ed1=True
+bool_eg2=False
+
+keep_going=""
+while keep_going=="":
+    print("looping")
+    print("looping")
+    keep_going=input("Again?")
+print("all done")
+
+#functions go here
+def intro():
+    print()
+    print("This is the function")
+    print()
+    print("Instructions go here")
+    return"" #I am returning (displaying/using) all items in the code
+#Main Routine
+intro() # This calls the functions so that we can use it.
+
+print("string_eg1,This is a string")
