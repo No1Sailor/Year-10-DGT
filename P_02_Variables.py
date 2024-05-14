c45ad1c63a70ad78d02a849d6a281e133a0e306e
No1Sailor <168596679+No1Sailor@users.noreply.github.com>
2024-05-15 09:41:43 +1200

    Add files via upload
    This is a file where we were learning more about variables and the rules we need to follow.

---
+++ P_02_Variables.py
@@ -0,0 +1,12 @@
+# P 02 Variables
+
+# Variables can be a string (note the speech marks)
+greeting = "Hello world"
+
+# Variables can be a number
+fav_num = 7
+num_string = 5
+
+print(fav_num * 2}
+print (num_string * 5)
+
